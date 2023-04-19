a7cf689faffa44ff5c41a302a1fd12ff19727db9
unknown <joseph.williams9210@gmail.com>
2023-04-18 21:45:31 -0500

    Add playRound function

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -8,7 +8,7 @@
 
 // Generates the Computer's choice
 const getComputerChoice = function() {
-    // Randomly generate a number between 0 & 2 and assign it to the variable computerChoice
+    // Randomly generate a number between 1 & 3 and assign it to the variable computerChoice
     let computerChoice = Math.floor(Math.random() * 3) + 1;
     // console.log(computerChoice) // Used to test the above expression
     if(computerChoice === 1) {
@@ -37,3 +37,36 @@ const getPlayerChoice = function() {
     return playerChoice
 }
 // console.log(getPlayerChoice()) // Used to test if function works
+
+
+// Pseudocode for a round of Rock, Paper, Scissors
+// PROGRAM playRound
+//     WHEN player inputs their selection:
+//         IF the player's choice beats the computer's choice,
+//             'You win!'
+//         ELSE IF the computer's choice beats the player's choice,
+//             'You lose!'
+//         ELSE,
+//             'Tie game!'
+// END
+
+const playRound = function(playerSelection, computerSelection) {
+    // If the player's choice beats the computer's choice
+    if((playerSelection === 'rock' && computerSelection === 'scissors') || (playerSelection === 'paper' && computerSelection === 'rock') || (playerSelection === 'scissors' && computerSelection === 'paper')) {
+        return `${playerSelection.charAt(0).toUpperCase()+playerSelection.slice(1)} beats ${computerSelection}. You win! 🏆`;
+    // If the computer's choice beats the player's choice
+    } else if((computerSelection === 'rock' && playerSelection === 'scissors') || (computerSelection === 'paper' && playerSelection === 'rock') || (computerSelection === 'scissors' && playerSelection === 'paper')) {
+        return `${computerSelection.charAt(0).toUpperCase()+computerSelection.slice(1)} beats ${playerSelection}. You lose 😢`;
+    // If the player's choice and the computer's choice is the same
+    } else if(playerSelection === computerSelection) {
+        return 'Tie game'
+    // If neither rock, paper, or scissors was entered as the player's choice
+    } else {
+        return 'Please enter rock, paper, or scissors.'
+    }
+}
+const computerSelection = getComputerChoice();
+console.log(computerSelection)
+const playerSelection = getPlayerChoice();
+console.log(playerSelection)
+console.log(playRound(playerSelection, computerSelection));
