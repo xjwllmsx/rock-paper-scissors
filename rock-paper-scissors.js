97fc7f4f57488e0b2f9a897570b3677ba4822c9d
Joseph Williams <josewilliams@meta.com>
2023-04-20 13:10:59 -0500

    Start game function I left comments explaining what each part of the function does, how it behaves, and any issues.
    This is a great start but there are changes that need to be made.

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -57,23 +57,26 @@ const playRound = function(playerSelection, computerSelection) {
     console.log(`Computer chose: ${computerSelection.charAt(0).toUpperCase()+computerSelection.slice(1)}`)
     // If the player's choice beats the computer's choice
     if((playerSelection === 'rock' && computerSelection === 'scissors') || (playerSelection === 'paper' && computerSelection === 'rock') || (playerSelection === 'scissors' && computerSelection === 'paper')) {
-        return `${playerSelection.charAt(0).toUpperCase()+playerSelection.slice(1)} beats ${computerSelection}. You win! 🏆`;
+        console.log(`${playerSelection.charAt(0).toUpperCase()+playerSelection.slice(1)} beats ${computerSelection}. You win! 🏆`)
+        return 'win';
     // If the computer's choice beats the player's choice
     } else if((computerSelection === 'rock' && playerSelection === 'scissors') || (computerSelection === 'paper' && playerSelection === 'rock') || (computerSelection === 'scissors' && playerSelection === 'paper')) {
-        return `${computerSelection.charAt(0).toUpperCase()+computerSelection.slice(1)} beats ${playerSelection}. You lose 😢`;
+        console.log(`${computerSelection.charAt(0).toUpperCase()+computerSelection.slice(1)} beats ${playerSelection}. You lose 😢`)
+        return 'lose';
     // If the player's choice and the computer's choice is the same
     } else if(playerSelection === computerSelection) {
-        return 'Tie game'
+        console.log('Tie game')
+        return 'tie'
     // If neither rock, paper, or scissors was entered as the player's choice
     } else {
         return 'Please enter rock, paper, or scissors.'
     }
 }
-const computerSelection = getComputerChoice();
+// const computerSelection = getComputerChoice();
 // console.log(computerSelection)
-const playerSelection = getPlayerChoice();
+// const playerSelection = getPlayerChoice();
 // console.log(playerSelection)
-console.log(playRound(playerSelection, computerSelection));
+// console.log(playRound(playerSelection, computerSelection));
 
 
 // Pseudocode for game function
@@ -84,3 +87,39 @@ console.log(playRound(playerSelection, computerSelection));
 //     If playerWins > computerWins THEN player wins;
 //     ELSE computerWins > playerWins THEN computer wins;
 // END
+
+// The game function below is a good start, but the issue I'm having right now is that every time the letsPlay function runs, it runs with the exact same computerSelection and playerSelection, so the two games are exactly the same. I need to figure out how to make the second round run with different selections. The player will need to be prompted again.
+const game = function() {
+    // A variable for the computer's selection
+    const computerSelection = getComputerChoice();
+    // A variable for the player's selection
+    const playerSelection = getPlayerChoice();
+    // Player's initial score
+    let playerScore = 0;
+    // Computer's initial score
+    let computerScore = 0;
+
+    // This function runs a round of rock, paper, scissors and outputs a score
+    const letsPlay = function (round) {
+        if(round === 'win') {
+            return playerScore = ++playerScore;
+        } else if (round === 'lose') {
+            return computerScore = ++computerScore;
+        } else {
+            playerScore = playerScore + 0
+            computerScore = computerScore + 0
+        }
+    };
+    // Calls the letsPlay function and displays it in the console
+    console.log(letsPlay(playRound(playerSelection, computerSelection)))
+    console.log(playerScore) // Outputs the player's score after the first round
+    console.log(computerScore) // Outputs the computer's score after the first round
+
+    // Calls the letsPlay function a second time
+    console.log(letsPlay(playRound(playerSelection, computerSelection)))
+    console.log(playerScore) // If the player won, the score will increase
+    console.log(computerScore) // If the computer won, the score will increase
+}
+
+// Calls the game function and displays in the console.
+console.log(game()); // undefined
